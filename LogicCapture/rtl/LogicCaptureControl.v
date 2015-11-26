4efcb7d2cfcffc91a5fcb7b8ea20c6d7235a122d
manic-moose <father.m0ose@gmail.com>
2015-11-25 19:06:30 -0800

    only changed formatting. module needs re-work to get inline with current implementation of picoblaze + hub

--- LogicCapture/rtl/LogicCaptureControl.v
+++ LogicCapture/rtl/LogicCaptureControl.v
@@ -1,7 +1,7 @@
 module LogicCaptureControl #(
-    parameter CPU_DW           = 8,
-    parameter SRAM_DW         = 16,
-    parameter SRAM_AW         = 16,
+    parameter CPU_DW       = 8,
+    parameter SRAM_DW      = 16,
+    parameter SRAM_AW      = 16,
     parameter SAMPLE_WIDTH = 8
 ) (
     input  clock,
@@ -12,11 +12,11 @@ module LogicCaptureControl #(
 
     // CPU Interface
     input  [CPU_DW-1:0] cpu_address,
-    input cpu_rdnwr, // True is read, false is write
+    input  cpu_rdnwr, // True is read, false is write
     inout  [CPU_DW-1:0]  cpu_data,
     
     //SRAM Interface Signals
-    inout   [SRAM_DW-1:0] sram_data,
+    inout  [SRAM_DW-1:0] sram_data,
     output [SRAM_AW-1:0] sram_address,
     output sram_oe,
     output sram_ce,
@@ -42,23 +42,23 @@ module LogicCaptureControl #(
     reg [CONFIG_REG_WIDTH-1:0] statusRegister;
     
     // Define which registers belong to which configuration or control mechanism
-    localparam START_REG                             = 0;
-    localparam ABORT_REG                             = 1;
-    localparam ACTIVE_CHANNELS_REG           = 2;
-    localparam EDGE_CHANNEL_REG                = 3;
-    localparam EDGE_TYPE_REG                      = 4;
-    localparam EDGE_TRIGGER_ENABLED_REG = 5;
-    localparam DESIRED_PATTERN_REG           = 6;
-    localparam DONT_CARE_REG                     = 7;
+    localparam START_REG                 = 0;
+    localparam ABORT_REG                 = 1;
+    localparam ACTIVE_CHANNELS_REG       = 2;
+    localparam EDGE_CHANNEL_REG          = 3;
+    localparam EDGE_TYPE_REG             = 4;
+    localparam EDGE_TRIGGER_ENABLED_REG  = 5;
+    localparam DESIRED_PATTERN_REG       = 6;
+    localparam DONT_CARE_REG             = 7;
     localparam PATTERN_TRIGG_ENABLE_REG  = 8;
-    localparam STATUS_REG                            = 9;
+    localparam STATUS_REG                = 9;
     
     // Assign configuration outputs to the desired registers
-    assign activeChannels          = configRegisters[ACTIVE_CHANNELS_REG][SAMPLE_WIDTH-1:0];
-    assign edgeChannel              = configRegisters[EDGE_CHANNEL_REG];
-    assign edgeType                  = configRegisters[EDGE_TYPE_REG][0];
+    assign activeChannels        = configRegisters[ACTIVE_CHANNELS_REG][SAMPLE_WIDTH-1:0];
+    assign edgeChannel           = configRegisters[EDGE_CHANNEL_REG];
+    assign edgeType              = configRegisters[EDGE_TYPE_REG][0];
     assign edgeTriggerEnabled    = configRegisters[EDGE_TRIGGER_ENABLED_REG][0];
-    assign desiredPattern            = configRegisters[DESIRED_PATTERN_REG][SAMPLE_WIDTH-1:0];
+    assign desiredPattern        = configRegisters[DESIRED_PATTERN_REG][SAMPLE_WIDTH-1:0];
     assign dontCareChannels      = configRegisters[DONT_CARE_REG][SAMPLE_WIDTH-1:0];
     assign patternTriggerEnabled = configRegisters[PATTERN_TRIGG_ENABLE_REG][0];
     
