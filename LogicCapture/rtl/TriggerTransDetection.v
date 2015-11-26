3079d8be01a3a25f1c2466ae5d0b456bd30626e1
manic-moose <father.m0ose@gmail.com>
2015-11-25 19:03:40 -0800

    added documentatino to trigger detection module

--- LogicCapture/rtl/TriggerTransDetection.v
+++ LogicCapture/rtl/TriggerTransDetection.v
@@ -1,3 +1,47 @@
+/* TriggerTransDetection.v - This module provides
+ * combinational logic on the sample input to
+ * detect different conditions that we care about.
+ *
+ * Triggers -
+ *   Edge Triggers - Uses current sample, previous sample,
+ *   and the edge trigger configuration to output a signal
+ *   indicating if an edge trigger is "triggered". If edge
+ *   trigger is disabled, the edgeTrigger is considered always
+ *   triggered.
+ *
+ *   Pattern Trigger - Compares the current sample
+ *   with a desired pattern and configuration to output
+ *   if a pattern has been detected. If the pattern trigger is
+ *   disabled, then this trigger is considered always triggered.
+ *
+ *   The final "triggered" output is true if both the edge and
+ *   pattern trigger conditions are satisfied. Note, that since
+ *   this module provides only combinational logic, the surrounding
+ *   sequential logic must capture the triggered signal and react
+ *   accordingly.
+ *
+ * Transition -
+ *   The transition signal is asserted whenever the active channels
+ *   of the current sample and the previous sample are not equal.
+ *
+ * Parameters:
+ *   SAMPLE_WIDTH - The total number of channels supported
+ *
+ * Inputs:
+ *   latestSample          - The current sample
+ *   previousSample        - The last sample
+ *   activeChannels        - Each bit indicates whether a channel is active or not
+ *   edgeChannel           - Indicates the channel that the edge trigger applies to.
+ *   edgeType              - 1 = positive edge trigger, 0 = negative edge trigger
+ *   edgeTriggerEnabled    - Enable/disable for the edge trigger
+ *   desiredPattern        - The desired value to be observed on active channels
+ *   dontCareChannels      - If a channel is active, but the pattern is not considered for the trigger
+ *   patternTriggerEnabled - Enable/disable for the pattern trigger
+ * Ouputs:
+ *   triggered  - Indicates a configured trigger is active
+ *   transition - Indicates a transition occurred on at least 1 active channel
+ *
+ */
 module TriggerTransDetection #(
     parameter SAMPLE_WIDTH = 8
 ) (
