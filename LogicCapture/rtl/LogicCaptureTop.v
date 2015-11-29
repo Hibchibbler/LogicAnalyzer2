a63f08c6171c96bef764b7e43d421f46b6fb803e
manic-moose <father.m0ose@gmail.com>
2015-11-28 22:33:19 -0800

    factored the command structure of LogicCaptureTop. same architecture, just factored into tasks for easy modification

--- LogicCapture/rtl/LogicCaptureTop.v
+++ LogicCapture/rtl/LogicCaptureTop.v
@@ -48,6 +48,7 @@ module LogicCaptureTop #(
     input                            pageFull
 );
 
+// Function code definitions
 localparam  CMD_NOP                 = 8'h00,
             CMD_START               = 8'h01,
             CMD_ABORT               = 8'h02,
@@ -56,7 +57,8 @@ localparam  CMD_NOP                 = 8'h00,
             CMD_READ_TRACE_DATA     = 8'h05,
             CMD_READ_TRACE_SIZE     = 8'h06,
             CMD_READ_TRIGGER_SAMP   = 8'h07,
-            CMD_GOT_DATA            = 8'h08;
+            CMD_ACK                 = 8'h08,
+            CMD_RESET               = 8'h09;
 
 reg [SAMPLE_WIDTH-1:0] sampleData_sync0;
 reg [SAMPLE_WIDTH-1:0] sampleData_sync1;
@@ -64,6 +66,10 @@ reg [SAMPLE_WIDTH-1:0] sampleData;
 
 reg start;
 reg abort;
+reg cmdReset;
+reg acknowledge;
+wire logCapReset;
+assign logCapReset = reset | cmdReset;
 
 /* Local Configuration Registers */
 /* - Buffer Configuration - */
@@ -79,73 +85,41 @@ reg [7:0]              edgeChannel;
 reg                    edgeType;
 reg                    edgeTriggerEnable;
 
+// some status data
+wire [31:0] sampleNumber_Begin,
+wire [31:0] sampleNumber_End,
+wire [31:0] sampleNumber_Trig,
+wire [31:0] traceSizeBytes
 wire postTrigger,preTrigger,idle;
+
 // assign the status register
-assign status = {5'b00000, postTrigger, preTrigger, idle};
+assign status = {4'b0000, acknowledge, postTrigger, preTrigger, idle};
 
 always @(posedge clk) begin
-    if (reset == 1'b1) begin
-        desiredPattern        <= {SAMPLE_WIDTH{1'b0}};
-        activeChannels        <= {SAMPLE_WIDTH{1'b0}};
-        dontCareChannels      <= {SAMPLE_WIDTH{1'b0}};
-        edgeChannel           <= 32'd0;
-        patternTriggerEnable  <= 1'b0;
-        edgeTriggerEnable     <= 1'b0;
-        edgeType              <= 1'b0;
-        currentCommand        <= CMD_NOP;
-        maxSampleCount        <= 32'd0;
-        preTriggerSampleCountMax <= 32'd0;
+    if (reset) begin
+        resetMe;
     end else begin
         if (command_strobe) begin
-            case (command)
-                CMD_START: currentCommand    <= CMD_START;
-                CMD_ABORT: currentCommand    <= CMD_ABORT;
-                CMD_TRIGGER_CONFIGURE: begin
-                    currentCommand           <= CMD_TRIGGER_CONFIGURE;
-                    desiredPattern           <= {regIn1, regIn0};
-                    activeChannels           <= {regIn3, regIn2};
-                    dontCareChannels         <= {regIn5, regIn4};
-                    edgeChannel              <= regIn6;
-                    patternTriggerEnable     <= regIn7[0];
-                    edgeTriggerEnable        <= regIn7[1];
-                    edgeType                 <= regIn7[2];
-                end
-                CMD_BUFFER_CONFIGURE: begin
-                    currentCommand           <= CMD_BUFFER_CONFIGURE;
-                    maxSampleCount           <= {regIn3, regIn2, regIn1, regIn0};
-                    preTriggerSampleCountMax    <= {regIn7, regIn6, regIn5, regIn4};
-                end
-                CMD_READ_TRACE_DATA: begin
-                    currentCommand           <= CMD_READ_TRACE_DATA;
-                    regOut0                  <= 8'hAA;//data we are uploading....
-                    regOut1                  <= 8'hBB;
-                    regOut2                  <= 8'hCC;
-                    regOut3                  <= 8'hDD;
-                    regOut4                  <= 8'hAA;
-                    regOut5                  <= 8'hBB;
-                    regOut6                  <= 8'hCC;
-                    regOut7                  <= 8'hDD;
-                end
-                default: begin
-                    currentCommand           <= CMD_NOP;
-                end
-            endcase
+            currentCommand <= command;
         end else begin
-            currentCommand                   <= CMD_NOP;
+            currentCommand <= CMD_NOP;
         end
     end
 end
 
-// Handle setting and clearing start/abort bits
+// Handle start/abort/reset pulses into LogCap
 always @(posedge clk) begin
     if (reset) begin
-        start <= 1'b0;
-        abort <= 1'b0;
+        start    <= 1'b0;
+        abort    <= 1'b0;
+        cmdReset <= 1'b0;
     end else begin
         case(currentCommand)
             CMD_START: start <= 1'b1;
-            CMD_ABORT: abort <= 1'b1
+            CMD_ABORT: abort <= 1'b1;
+            CMD_RESET: cmdReset <= 1'b1;
             default:   begin
+                         cmdReset <= 1'b0;
                          // Hold abort until
                          // in the idle state
                          if (abort) begin
@@ -154,7 +128,7 @@ always @(posedge clk) begin
                             end else begin
                                 abort <= 1'b1;
                             end
-                         end else ebgin
+                         end else begin
                             abort <= 1'b0;
                          end
                          start <= 1'b0;
@@ -162,6 +136,46 @@ always @(posedge clk) begin
         endcase
     end
 end
+
+always @(posedge clk) begin
+    if (~reset) begin
+        executeCommand;
+    end
+end
+
+task resetMe;
+begin
+    desiredPattern           <= {SAMPLE_WIDTH{1'b0}};
+    activeChannels           <= {SAMPLE_WIDTH{1'b0}};
+    dontCareChannels         <= {SAMPLE_WIDTH{1'b1}};
+    edgeChannel              <= 32'd0;
+    patternTriggerEnable     <= 1'b0;
+    edgeTriggerEnable        <= 1'b0;
+    edgeType                 <= 1'b0;
+    currentCommand           <= CMD_NOP;
+    maxSampleCount           <= 32'd100;
+    preTriggerSampleCountMax <= 32'd0;
+end
+endtask
+
+task executeCommand;
+begin
+   case (command)
+        CMD_START:               executeStart;
+        CMD_ABORT:               executeAbort;
+        CMD_TRIGGER_CONFIGURE:   executeConfigTrigger
+        CMD_BUFFER_CONFIGURE:    executeConfigBuffer;
+        CMD_READ_TRACE_DATA:     executeReadTraceData
+        CMD_ACK:                 clearAck;
+        CMD_READ_TRACE_SIZE:     executeReadTraceSize;
+        CMD_READ_TRIGGER_SAMPLE: executeReadTriggerSample;
+        CMD_RESET:               executeReset;
+        default: begin
+            currentCommand <= CMD_NOP;
+        end
+    endcase
+end
+endtask
 
 // Synchronize sample inputs to this clock domain
 always @(posedge clk) begin
@@ -175,7 +189,7 @@ LogCap #(
     .SAMPLE_PACKET_WIDTH(SAMPLE_PACKET_WIDTH)
 ) ilogcap (
     .clk(clk),
-    .reset(reset),
+    .reset(logCapReset),
     .sampleData(sampleData),
     .maxSampleCount(maxSampleCount),
     .preTriggerSampleCountMax(preTriggerSampleCountMax),
@@ -195,7 +209,93 @@ LogCap #(
     .samplePacket(samplePacket),
     .write_enable(write_enable),
     .sample_number(sample_number),
-    .pageFull(pageFull)
+    .pageFull(pageFull),
+    .sampleNumber_Begin(sampleNumber_Begin),
+    .sampleNumber_End(sampleNumber_End),
+    .sampleNumber_Trig(sampleNumber_Trig),
+    .traceSizeBytes(traceSizeBytes)
 );
+
+/*  BEGIN COMMAND TASK DEFINITIONS  */
+
+task clearAck;
+begin
+    acknowledge = 1'b0;
+end
+endtask
+
+task acknowledgeCmd;
+begin
+    acknowledge <= 1'b1;
+end
+endtask
+
+task executeReset;
+begin
+    resetMe;
+    acknowledgeCmd;
+end
+endtask
+
+task executeReadTraceSize;
+begin
+    regOut3 <= traceSizeBytes[31:24];
+    regOut2 <= traceSizeBytes[23:16];
+    regOut1 <= traceSizeBytes[15:8];
+    regOut0 <= traceSizeBytes[7:0];
+    acknowledgeCmd;
+end
+endtask
+
+task executeReadTriggerSample;
+begin
+    regOut3 <= sampleNumber_Trig[31:24];
+    regOut2 <= sampleNumber_Trig[23:16];
+    regOut1 <= sampleNumber_Trig[15:8];
+    regOut0 <= sampleNumber_Trig[7:0];
+    acknowledgeCmd;
+end
+endtask
+
+task executeReadTraceData;
+begin
+
+end
+endtask
+
+task executeConfigBuffer;
+begin
+    currentCommand           <= CMD_BUFFER_CONFIGURE;
+    maxSampleCount           <= {regIn3, regIn2, regIn1, regIn0};
+    preTriggerSampleCountMax <= {regIn7, regIn6, regIn5, regIn4};
+    ackowledgeCmd;
+end
+endtask;
+
+task executeConfigTrigger;
+begin
+    currentCommand       <= CMD_TRIGGER_CONFIGURE;
+    desiredPattern       <= {regIn1, regIn0};
+    activeChannels       <= {regIn3, regIn2};
+    dontCareChannels     <= {regIn5, regIn4};
+    edgeChannel          <= regIn6;
+    patternTriggerEnable <= regIn7[0];
+    edgeTriggerEnable    <= regIn7[1];
+    edgeType             <= regIn7[2];
+    acknowledgeCmd;
+end
+endtask
+
+task executeStart;
+begin
+    acknowledgeCmd;
+end
+endtask
+
+task executeAbort;
+begin
+    acknowledgeCmd;
+end
+endtask
  
 endmodule
