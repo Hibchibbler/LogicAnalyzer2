07074be70f3f6cad97e284236a5719e5d12f651f
manic-moose <father.m0ose@gmail.com>
2015-12-03 15:09:44 -0800

    fixed timescale so that it works with ddr stuff correctly. fixed some syntax errors and clocking stuff

--- Integration/Kernel/tb/testbench.v
+++ Integration/Kernel/tb/testbench.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 100ps
+`timescale 1ps/100fs
 
 // -----------------------------------------------------------------
 // Version:  0.0
@@ -14,39 +14,41 @@
 `define TB_MODE 1
 `define RESET 0
 
-   /************************ PARAMETERS  *************************/
-    parameter CLOCK_PERIOD_IN  = 10000;  // 10000 ps - 100 MHz base clock.
-    parameter RESET_PERIOD     = 20000; //  20000 ps reset delay
-    
-    // Some parameters for proper instantiation of DDR FBM
-    localparam MEMORY_WIDTH    = 16;
-    localparam DQ_WIDTH        = 16;                      // # of DQ (data)
-    localparam NUM_COMP        = DQ_WIDTH/MEMORY_WIDTH;
-    localparam CS_WIDTH        = 1;                       // # of unique CS outputs to memory.
-    localparam DM_WIDTH        = 2;                       // # of DM (data mask)
-    localparam DQS_WIDTH       = 2;
-    parameter  ROW_WIDTH       = 13;                      // # of memory Row Address bits.
-    parameter  BANK_WIDTH      = 3;                       // # of memory Bank Address bits.
-    parameter  CKE_WIDTH       = 1;                       // # of CKE outputs to memory.
-    parameter  CK_WIDTH        = 1;                       // # of CK/CK# outputs to memory.
-    parameter  ODT_WIDTH       = 1;                       // # of ODT outputs to memory.
-    parameter  nCS_PER_RANK    = 1;                       // # of unique CS outputs per rank for phy
-    
-    // wire delay parameters
-    localparam real TPROP_PCB_DATA     = 0.00; // Delay for data signal during Write operation
-    localparam real TPROP_PCB_DATA_RD  = 0.00; // Delay for data signal during Read operation
-    localparam real TPROP_DQS          = 0.00; // Delay for DQS signal during Write Operation
-    localparam real TPROP_DQS_RD       = 0.00; // Delay for DQS signal during Read Operation
-    localparam real TPROP_PCB_CTRL     = 0.00; // Delay for Address and Ctrl signals
-    parameter  ECC                     = "OFF";
-    localparam ECC_TEST 		   	   = "OFF" ;
-    localparam ERR_INSERT              = (ECC_TEST == "ON") ? "OFF" : ECC ;
-    /************************ END PARAMETERS **********************/
 
 // testbench for command and control + hub
 module testbench();
 
-reg         clk;                            // clock of nexys4fpga
+/************************ PARAMETERS  *************************/
+parameter CLOCK_PERIOD_IN  = 10000;    // 10000 ps - 100 MHz base clock.
+parameter RESET_PERIOD     = 20000;    //  20000 ps reset delay
+parameter INIT_DELAY       = 80000000; // 80 usec delay before issuing commands
+
+// Some parameters for proper instantiation of DDR FBM
+localparam MEMORY_WIDTH    = 16;
+localparam DQ_WIDTH        = 16;                      // # of DQ (data)
+localparam NUM_COMP        = DQ_WIDTH/MEMORY_WIDTH;
+localparam CS_WIDTH        = 1;                       // # of unique CS outputs to memory.
+localparam DM_WIDTH        = 2;                       // # of DM (data mask)
+localparam DQS_WIDTH       = 2;
+parameter  ROW_WIDTH       = 13;                      // # of memory Row Address bits.
+parameter  BANK_WIDTH      = 3;                       // # of memory Bank Address bits.
+parameter  CKE_WIDTH       = 1;                       // # of CKE outputs to memory.
+parameter  CK_WIDTH        = 1;                       // # of CK/CK# outputs to memory.
+parameter  ODT_WIDTH       = 1;                       // # of ODT outputs to memory.
+parameter  nCS_PER_RANK    = 1;                       // # of unique CS outputs per rank for phy
+
+// wire delay parameters
+localparam real TPROP_PCB_DATA     = 0.00; // Delay for data signal during Write operation
+localparam real TPROP_PCB_DATA_RD  = 0.00; // Delay for data signal during Read operation
+localparam real TPROP_DQS          = 0.00; // Delay for DQS signal during Write Operation
+localparam real TPROP_DQS_RD       = 0.00; // Delay for DQS signal during Read Operation
+localparam real TPROP_PCB_CTRL     = 0.00; // Delay for Address and Ctrl signals
+parameter  ECC                     = "OFF";
+localparam ECC_TEST 		   	   = "OFF" ;
+localparam ERR_INSERT              = (ECC_TEST == "ON") ? "OFF" : ECC ;
+/************************ END PARAMETERS **********************/
+
+reg  clk;                               // onboard oscillator
 reg         btnW, btnE, btnN, btnS, btnC,   // buttons of nexys4fpga
             btnCpuReset;
 reg  [15:0] sw;                             // switches on nexys4fpga
@@ -69,45 +71,55 @@ wire       utx_buffer_full;
 wire       utx_buffer_half_full;
 wire       utx_buffer_data_present;
 
-      // DDR Memory Wires
-      // memory model - signals into BFM, delayed version of *fpga
-      wire [DQ_WIDTH-1:0]                ddr2_dq_sdram;
-      reg  [ROW_WIDTH-1:0]               ddr2_addr_sdram;
-      reg  [BANK_WIDTH-1:0]              ddr2_ba_sdram;
-      reg                                ddr2_ras_n_sdram;
-      reg                                ddr2_cas_n_sdram;
-      reg                                ddr2_we_n_sdram;
-      wire [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_sdram;
-      wire [ODT_WIDTH-1:0]               ddr2_odt_sdram;
-      reg  [CKE_WIDTH-1:0]               ddr2_cke_sdram;
-      wire [DM_WIDTH-1:0]                ddr2_dm_sdram;
-      wire [DQS_WIDTH-1:0]               ddr2_dqs_p_sdram;
-      wire [DQS_WIDTH-1:0]               ddr2_dqs_n_sdram;
-      reg  [CK_WIDTH-1:0]                ddr2_ck_p_sdram;
-      reg  [CK_WIDTH-1:0]                ddr2_ck_n_sdram;
-      // fpga pad - signals direct out of controller
-      wire [DQ_WIDTH-1:0]                ddr2_dq_fpga;
-      wire [DQS_WIDTH-1:0]               ddr2_dqs_p_fpga;
-      wire [DQS_WIDTH-1:0]               ddr2_dqs_n_fpga;
-      wire [ROW_WIDTH-1:0]               ddr2_addr_fpga;
-      wire [BANK_WIDTH-1:0]              ddr2_ba_fpga;
-      wire                               ddr2_ras_n_fpga;
-      wire                               ddr2_cas_n_fpga;
-      wire                               ddr2_we_n_fpga;
-      wire [CKE_WIDTH-1:0]               ddr2_cke_fpga;
-      wire [CK_WIDTH-1:0]                ddr2_ck_p_fpga;
-      wire [CK_WIDTH-1:0]                ddr2_ck_n_fpga;
-      wire [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_fpga;
-      wire [DM_WIDTH-1:0]                ddr2_dm_fpga;
-      wire [ODT_WIDTH-1:0]               ddr2_odt_fpga;
-      reg  [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_sdram_tmp;
-      reg  [DM_WIDTH-1:0]                ddr2_dm_sdram_tmp;
-      reg  [ODT_WIDTH-1:0]               ddr2_odt_sdram_tmp;
+// DDR Memory Wires
+// memory model - signals into BFM, delayed version of *fpga
+wire [DQ_WIDTH-1:0]                ddr2_dq_sdram;
+reg  [ROW_WIDTH-1:0]               ddr2_addr_sdram;
+reg  [BANK_WIDTH-1:0]              ddr2_ba_sdram;
+reg                                ddr2_ras_n_sdram;
+reg                                ddr2_cas_n_sdram;
+reg                                ddr2_we_n_sdram;
+wire [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_sdram;
+wire [ODT_WIDTH-1:0]               ddr2_odt_sdram;
+reg  [CKE_WIDTH-1:0]               ddr2_cke_sdram;
+wire [DM_WIDTH-1:0]                ddr2_dm_sdram;
+wire [DQS_WIDTH-1:0]               ddr2_dqs_p_sdram;
+wire [DQS_WIDTH-1:0]               ddr2_dqs_n_sdram;
+reg  [CK_WIDTH-1:0]                ddr2_ck_p_sdram;
+reg  [CK_WIDTH-1:0]                ddr2_ck_n_sdram;
+// fpga pad - signals direct out of controller
+wire [DQ_WIDTH-1:0]                ddr2_dq_fpga;
+wire [DQS_WIDTH-1:0]               ddr2_dqs_p_fpga;
+wire [DQS_WIDTH-1:0]               ddr2_dqs_n_fpga;
+wire [ROW_WIDTH-1:0]               ddr2_addr_fpga;
+wire [BANK_WIDTH-1:0]              ddr2_ba_fpga;
+wire                               ddr2_ras_n_fpga;
+wire                               ddr2_cas_n_fpga;
+wire                               ddr2_we_n_fpga;
+wire [CKE_WIDTH-1:0]               ddr2_cke_fpga;
+wire [CK_WIDTH-1:0]                ddr2_ck_p_fpga;
+wire [CK_WIDTH-1:0]                ddr2_ck_n_fpga;
+wire [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_fpga;
+wire [DM_WIDTH-1:0]                ddr2_dm_fpga;
+wire [ODT_WIDTH-1:0]               ddr2_odt_fpga;
+reg  [(CS_WIDTH*nCS_PER_RANK)-1:0] ddr2_cs_n_sdram_tmp;
+reg  [DM_WIDTH-1:0]                ddr2_dm_sdram_tmp;
+reg  [ODT_WIDTH-1:0]               ddr2_odt_sdram_tmp;
 
 integer i;
 
+// Generate source clock
+initial clk = 1'b0;
+always clk = #(CLOCK_PERIOD_IN/2.0) ~clk;
+
+// RESETS
+// Generate soc reset
 initial begin
-    clk = 1'b0;
+    btnCpuReset = 1'b0;
+    #RESET_PERIOD btnCpuReset = 1'b1;
+end
+
+initial begin
     {btnW, btnE, btnN, btnS, btnC} = 5'h00;
     sw = 16'h0000;
     data_tx = 8'h00;
@@ -115,9 +127,6 @@ initial begin
     JA = 8'h00;
     JB = 8'h00;
 end
-
-// 100MHz clock
-always #5 clk = ~clk;
 
 uart_rx6 uart_rx
 (
@@ -166,21 +175,21 @@ nexys4fpga #(.TB_MODE(1)) target
     .uart_txd(uart_rxd),
     .JA(JA),
     .JB(JB),
-        // DDR2 Pins
-        .ddr2_dq(ddr2_dq_fpga),
-        .ddr2_dqs_n(ddr2_dqs_n_fpga),
-        .ddr2_dqs_p(ddr2_dqs_p_fpga),
-        .ddr2_addr(ddr2_addr_fpga),
-        .ddr2_ba(ddr2_ba_fpga),
-        .ddr2_ras_n(ddr2_ras_n_fpga),
-        .ddr2_cas_n(ddr2_cas_n_fpga),
-        .ddr2_we_n(ddr2_we_n_fpga),
-        .ddr2_ck_p(ddr2_ck_p_fpga),
-        .ddr2_ck_n(ddr2_ck_n_fpga),
-        .ddr2_cke(ddr2_cke_fpga),
-        .ddr2_cs_n(ddr2_cs_n_fpga),
-        .ddr2_dm(ddr2_dm_fpga),
-        .ddr2_odt(ddr2_odt_fpga)
+    // DDR2 Pins
+    .ddr2_dq(ddr2_dq_fpga),
+    .ddr2_dqs_n(ddr2_dqs_n_fpga),
+    .ddr2_dqs_p(ddr2_dqs_p_fpga),
+    .ddr2_addr(ddr2_addr_fpga),
+    .ddr2_ba(ddr2_ba_fpga),
+    .ddr2_ras_n(ddr2_ras_n_fpga),
+    .ddr2_cas_n(ddr2_cas_n_fpga),
+    .ddr2_we_n(ddr2_we_n_fpga),
+    .ddr2_ck_p(ddr2_ck_p_fpga),
+    .ddr2_ck_n(ddr2_ck_n_fpga),
+    .ddr2_cke(ddr2_cke_fpga),
+    .ddr2_cs_n(ddr2_cs_n_fpga),
+    .ddr2_dm(ddr2_dm_fpga),
+    .ddr2_odt(ddr2_odt_fpga)
 );
 
 
@@ -190,88 +199,74 @@ assign urx_buffer_read = urx_buffer_data_present;
 // print any uart data coming out of the nexys4fpga
 always @(negedge urx_buffer_read) $display("%5d UART data out: %h", $time, data_rx);
 
-
-// actual test bench
 initial begin
-    // reset the system
-    #5   btnCpuReset =  `RESET;
-    #100 btnCpuReset = ~`RESET;
-
-    //for (i=0; i<100; i=i+1)
-    #1000 cmd_abort();
-    #100 cmd_write_trig_cfg({{56{1'b0},8'h55});
-    #100 cmd_read_trig_cfg();
-    
+    #INIT_DELAY
+//    #10 cmd_abort;
+    cmd_write_trig_cfg({{56{1'b0}},8'h55});
+    #(CLOCK_PERIOD_IN*2) cmd_read_trig_cfg;
     // takes a total of ~22uS to get the full "HELLO World" back
     #22000;
-    
-    
-    //$finish;
 end
 
 task cmd_start;
 begin
-    write_uart({64{1'b0}, 8'h01});
+    write_uart({{64{1'b0}}, 8'h01});
 end
 endtask
 
 task cmd_abort;
 begin
-    write_uart({64{1'b0}, 8'h02});
+    write_uart({{64{1'b0}}, 8'h02});
 end
 endtask
 
 task cmd_write_trig_cfg;
 input [63:0] trig_cfg;
 begin
-    write_uart(trig_cfg, 8'h03});
+    write_uart({trig_cfg, 8'h03});
 end
 endtask
 
-// maxSampleCount           <= {regIn3, regIn2, regIn1, regIn0};
-// preTriggerSampleCountMax <= {regIn7, regIn6, regIn5, regIn4};
-// 4 lower bytes = maxSampleCount
-// 4 upper bytes = preTriggerSampleCountMax
 task cmd_write_buff_cfg;
 input [63:0] buff_cfg;
 begin
-    write_uart(buff_cfg, 8'h04});
+    write_uart({buff_cfg, 8'h04});
 end
 endtask
 
 task cmd_read_trace_data;
 begin
-    write_uart({64{1'b0}, 8'h05});
+    write_uart({{64{1'b0}}, 8'h05});
 end
 endtask
 
 task cmd_read_trace_size;
 begin
-    write_uart({64{1'b0}, 8'h06});
+    write_uart({{64{1'b0}}, 8'h06});
 end
 endtask
 
 task cmd_read_trig_sample;
 begin
-    write_uart({64{1'b0}, 8'h07});
+    write_uart({{64{1'b0}}, 8'h07});
 end
 endtask
 
 task cmd_reset_logcap;
 begin
-    write_uart({64{1'b0}, 8'h09});
+    write_uart({{64{1'b0}}, 8'h09});
 end
 endtask
 
 task cmd_read_buff_cfg;
 begin
-    write_uart({64{1'b0}, 8'h0A});
+    write_uart({{64{1'b0}}, 8'h0A});
 end
 endtask
 
 task cmd_read_trig_cfg;
 begin
-    write_uart({64{1'b0}, 8'h0B});
+    write_uart({{64{1'b0}}, 8'h0B});
 end
 endtask
 
@@ -279,18 +274,18 @@ endtask
 
 task write_uart;
 input [71:0] data;
-integer count = 0;
+integer count;
 begin
     // make sure buffer isn't full
     //while (utx_buffer_full) 
-    
+    count = 0;
     while (count < 9) begin
         if (utx_buffer_full) begin // wait a cycle
             @(posedge clk);
         end 
         else begin
             @(posedge clk);
-                data_tx = data[count+7:count];
+                data_tx = data[count*8-+7-:8];
                 utx_buffer_write = 1'b1;
             @(posedge clk);
                 utx_buffer_write = 1'b0;
@@ -397,11 +392,11 @@ endtask
   /******************** END DELAY GENERATION **********************/
   
   // BFM instantiation
-  genvar r,i;
+  genvar r,q;
   generate
     for (r = 0; r < CS_WIDTH; r = r + 1) begin: mem_rnk
       if(DQ_WIDTH/16) begin: mem
-        for (i = 0; i < NUM_COMP; i = i + 1) begin: gen_mem
+        for (q = 0; q < NUM_COMP; q = q + 1) begin: gen_mem
           ddr2_model u_comp_ddr2
             (
              .ck      (ddr2_ck_p_sdram[0+(NUM_COMP*r)]),
@@ -411,12 +406,12 @@ endtask
              .ras_n   (ddr2_ras_n_sdram),
              .cas_n   (ddr2_cas_n_sdram),
              .we_n    (ddr2_we_n_sdram),
-             .dm_rdqs (ddr2_dm_sdram[(2*(i+1)-1):(2*i)]),
+             .dm_rdqs (ddr2_dm_sdram[(2*(q+1)-1):(2*q)]),
              .ba      (ddr2_ba_sdram),
              .addr    (ddr2_addr_sdram),
-             .dq      (ddr2_dq_sdram[16*(i+1)-1:16*(i)]),
-             .dqs     (ddr2_dqs_p_sdram[(2*(i+1)-1):(2*i)]),
-             .dqs_n   (ddr2_dqs_n_sdram[(2*(i+1)-1):(2*i)]),
+             .dq      (ddr2_dq_sdram[16*(q+1)-1:16*(q)]),
+             .dqs     (ddr2_dqs_p_sdram[(2*(q+1)-1):(2*q)]),
+             .dqs_n   (ddr2_dqs_n_sdram[(2*(q+1)-1):(2*q)]),
              .rdqs_n  (),
              .odt     (ddr2_odt_sdram[0+(NUM_COMP*r)])
              );
